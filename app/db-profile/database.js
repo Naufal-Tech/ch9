3df8d4fa887ff9bd9e63f2f7305e7a5fa5f6eb92
Naufal-Tech <naufal.geo11@gmail.com>
2022-07-26 12:35:10 +0700

    cobapush

--- app/db-profile/database.js
+++ app/db-profile/database.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "admin",
+  password: "123456",
   host: "localhost",
   port: 5432,
   database: "profile-page",
